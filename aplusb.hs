6202546aad89aab715fab736f64e362bebb61b69
rodlin <im.rod@outlook.com>
2017-11-09 15:34:39 +0800

    091117#01: add A + B = B + A

--- aplusb.hs
+++ aplusb.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE GADTs         #-}
-{-# LANGUAGE TypeFamilies  #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE GADTs, TypeFamilies, TypeOperators #-}
 
 module Kata.AdditionCommutes (plusCommutes) where
 
@@ -10,23 +8,6 @@ module Kata.AdditionCommutes (plusCommutes) where
 -- These are some lemmas that may be helpful.
 -- They will *not* be tested, so rename them
 -- if you so desire. Good luck!
-
--- | For any n, n = n.
-reflexive :: Natural n -> Equal n n
-reflexive = error "Optional: reflexive"
-
--- | if a = b, then b = a.
-symmetric :: Equal a b -> Equal b a
-symmetric = error "Optional: symmetric"
-
--- | if a = b and b = c, then a = c.
-transitive :: Equal a b -> Equal b c -> Equal a c
-transitive = error "Optional: transitive"
-
--- This is the proof that the kata requires.
--- | a + b = b + a
-plusCommutes :: Natural a -> Natural b -> Equal (a :+: b) (b :+: a)
-plusCommutes = error "TODO: plusCommutes"
 
 -- For reference, here are the definitions, if you
 -- want to copy them into an IDE:
@@ -50,3 +31,30 @@ data Equal :: * -> * -> * where
 type family (:+:) (n :: *) (m :: *) :: *
 type instance Z :+: m = m
 type instance S n :+: m = S (n :+: m)
+
+-- | For any n, n = n.
+reflexive :: Natural n -> Equal n n
+reflexive NumZ = EqlZ
+reflexive (NumS n) = EqlS (reflexive n)
+
+-- | if a = b, then b = a.
+symmetric :: Equal a b -> Equal b a
+symmetric EqlZ = EqlZ
+symmetric (EqlS a) = EqlS (symmetric a)
+
+-- | if a = b and b = c, then a = c.
+transitive :: Equal a b -> Equal b c -> Equal a c
+transitive EqlZ EqlZ = EqlZ
+transitive (EqlS a) (EqlS b) = EqlS (transitive a b)
+
+-- This is the proof that the kata requires.
+-- | a + b = b + a
+plusCommutes :: Natural a -> Natural b -> Equal (a :+: b) (b :+: a)
+plusCommutes NumZ NumZ = EqlZ
+plusCommutes NumZ (NumS b') = EqlS (plusCommutes NumZ b')
+plusCommutes (NumS a') NumZ = EqlS (plusCommutes a' NumZ)
+plusCommutes a@(NumS a') b@(NumS b') = let
+        lem1 = EqlS (plusCommutes a' b')
+        lem2 = plusCommutes a b'
+        lem3 = plusCommutes b a'
+    in symmetric (EqlS (transitive (symmetric lem2) (transitive lem1 lem3)))
